6694f7fd5b4151639a1a0a7a37ea48e6c5f3e986
jack-bots <75717012+vedamithra430@users.noreply.github.com>
2023-01-29 17:51:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "933710134"))
 PORT = os.environ.get("PORT", "69")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://shiva4656:sH@123456#@cluster0.js4pemo.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://filmy4u:tmqPqq11gozIE0g0@cluster0.rgsuzqt.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "Filmy4usbot")
 
 #force sub channel id, if you want enable force sub
