e16057ec111144397ca62d20ffb81172ba4a149b
jack-bots <75717012+vedamithra430@users.noreply.github.com>
2023-01-29 21:40:52 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://settings'>This Person</a>",
+            text = f"<b>○ Creator : <a href='tg://settings'>This Person</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
